8b02998d31c527c356b54b477798f8c003aef671
Aaron <dongin.kim@lemonhc.com>
2023-09-21 17:27:59 +0900

    import 정리 (ApiService)

--- src/main/java/com/dlsdlworld/spring/api/service/ApiService.java
+++ src/main/java/com/dlsdlworld/spring/api/service/ApiService.java
@@ -1,8 +1,6 @@
 package com.dlsdlworld.spring.api.service;
 
-import com.dlsdlworld.spring.api.dto.TestTable;
 import com.dlsdlworld.spring.api.repository.ApiRepository;
-import com.dlsdlworld.spring.api.repository.TestRepository;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
